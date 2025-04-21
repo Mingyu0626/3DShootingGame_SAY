7a772af285b2dff74a84873e3d5c124c2dc1c4a8
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-04-21 13:59:01 +0900

    Feat | 플레이어 회전 구현

---
+++ Assets/02. Scripts/02-2.Player/PlayerRotate.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed; // 플레이어의 회전 속도, 플레이어의 회전 속도는 카메라 회전 속도와 동일해야한다.
+
+    private float _rotationX = 0;
+    private void Awake()
+    {
+    }
+    private void Update()
+    {
+        Rotate();
+    }
+    private void Rotate()
+    {
+        // 1. 마우스 입력을 받는다.
+        float mouseX = Input.GetAxis("Mouse X");
+        // 2. 회전한 양만큼 누적시켜 나간다.
+        _rotationX += mouseX * _rotationSpeed * Time.deltaTime;
+        // 3. 회전 방향으로 회전시킨다.
+        transform.eulerAngles = new Vector3(0f, _rotationX, 0f);
+    }
+}
