28fda012a0e8fdf8d648138434bf00b7fc32bf33
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-04-22 13:02:14 +0900

    Feat | 플레이어가 발사할 폭탄 로직 구현

---
+++ Assets/02. Scripts/02-3.Weapon/Bomb.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    // 목표 : 마우스 우클릭을 하면, 카메라가 바라보는 방향으로 수류탄 던지기
+    // 1. 수류탄 오브젝트 만들기
+    // 2. 오른쪽 버튼 입력 받기
+    // 3. 발사 위치에 수류탄 생성하기
+    // 4. 생성된 수류탄을 카메라 방향으로 물리적인 힘 가하기
+    [Header("Project")]
+    [SerializeField]
+    private GameObject _explosionVFXPrefab;
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(_explosionVFXPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
