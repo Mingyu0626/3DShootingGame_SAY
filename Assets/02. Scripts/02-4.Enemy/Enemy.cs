91a2fa55c1ac659c9a399c999bea589cdca837d6
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-04-23 16:49:40 +0900

    Style | 미사용 EnemyState enum 제거

--- Assets/02. Scripts/02-4.Enemy/Enemy.cs
+++ Assets/02. Scripts/02-4.Enemy/Enemy.cs
@@ -1,15 +1,5 @@
 using UnityEngine;
 
-public enum EnemyState
-{
-    Idle,
-    Trace,
-    Return,
-    Attack,
-    Damaged,
-    Die
-}
-
 public class Enemy : MonoBehaviour, IProduct
 {
     public void Init()
