7ee0120764919fa8eb6b9cbb733a9bc05087d4fb
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-04-23 15:48:43 +0900

    Feat | 적 사망 State 구현

--- Assets/02. Scripts/02-4.Enemy/State/EnemyDieState.cs
+++ Assets/02. Scripts/02-4.Enemy/State/EnemyDieState.cs
@@ -1,12 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyDieState : IEnemyState
 {
     private EnemyController _enemyController;
+    private IEnumerator _dieCoroutine;
 
     public void Enter(EnemyController enemyController)
     {
         _enemyController = enemyController;
+        _dieCoroutine = DieCoroutine();
+        _enemyController.StartCoroutineInEnemyState(_dieCoroutine);
     }
 
     public void Update()
@@ -15,5 +20,17 @@ public class EnemyDieState : IEnemyState
 
     public void Exit()
     {
+        if (!ReferenceEquals(_dieCoroutine, null))
+        {
+            _enemyController.StopCoroutineInEnemyState(_dieCoroutine);
+            _dieCoroutine = null;
+        }
+    }
+
+    private IEnumerator DieCoroutine()
+    {
+        // TODO: Implement die logic (e.g., play death animation, drop items, etc.)
+        yield return new WaitForSeconds(2f);
+        EnemyPool.Instance.ReturnObject(_enemyController.GetComponent<Enemy>());
     }
 } 
