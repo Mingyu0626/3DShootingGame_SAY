7afcfa73a238f73bea150064ff9ae5a5192dd8b5
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-05-08 18:28:35 +0900

    Style | 기존 스크립트 이름과 충돌 우려가 있는 Pool 에셋 내 데모용 스크립트 Rename

--- Assets/20. External Assets/Pools Asset/Submodules/Pools/Demo/Scripts/EnemyDemo.cs
+++ Assets/20. External Assets/Pools Asset/Submodules/Pools/Demo/Scripts/EnemyDemo.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour, IPoolObject
+public class EnemyDemo : MonoBehaviour, IPoolObject
 {
     public enum State { Alive, Died }
     public int Health { get; private set; }
--- Assets/20. External Assets/Pools Asset/Submodules/Pools/Demo/Scripts/Test.cs
+++ Assets/20. External Assets/Pools Asset/Submodules/Pools/Demo/Scripts/Test.cs
@@ -9,20 +9,20 @@ public class Test : MonoBehaviour
     private PoolManager _poolManager;
 
     [SerializeField]
-    private Enemy _enemyPrefab;
+    private EnemyDemo _enemyPrefab;
 
     private void Start()
     {
         // Getting pool from pool manager.
         // Also has other overloads.
-        var enemyPool = _poolManager.GetPool<Enemy>();
+        var enemyPool = _poolManager.GetPool<EnemyDemo>();
 
         // Find pool of type Enemy and get object from it.
         // Also has other overloads.
-        var enemy = _poolManager.GetFromPool<Enemy>();
+        var enemy = _poolManager.GetFromPool<EnemyDemo>();
 
         // Find pool of type Enemy and returns clone to it.
         // Also has other overloads.
-        _poolManager.TakeToPool<Enemy>(enemy);
+        _poolManager.TakeToPool<EnemyDemo>(enemy);
     }
 }
