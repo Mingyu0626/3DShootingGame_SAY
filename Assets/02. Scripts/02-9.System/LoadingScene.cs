07a4618e1a70491f9665b44491d1311560edfc59
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-05-09 16:56:01 +0900

    Feat | 로딩씬 및 비동기 씬 전환 구현

---
+++ Assets/02. Scripts/02-9.System/LoadingScene.cs
@@ -0,0 +1,50 @@
+using TMPro;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadingScene : MonoBehaviour
+{
+    // 목표 : 다음 Scene을 '비동기 방식'으로 로드하고 싶다.
+    // 또한, 로딩 진행률을 시각적으로 표현하고 싶다. -> Slider와 % 텍스트 표시를 통해
+
+    private int _nextSceneIndex = 2;
+
+    [SerializeField]
+    private Slider _loadingSlider;
+
+    [SerializeField]
+    private TextMeshProUGUI _progressTMP;
+
+    private void Start()
+    {
+        StartCoroutine(LoadMainSceneAsync(_nextSceneIndex));
+    }
+
+    private IEnumerator LoadMainSceneAsync(int sceneIndex)
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneIndex);
+        asyncOperation.allowSceneActivation = false; // 비동기로 로드되는 Scene의 모습이 화면에 보이지 않게 한다.
+
+        while (!asyncOperation.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncOperation.progress / 0.9f);
+            _loadingSlider.value = asyncOperation.progress;
+            _progressTMP.text = $"{(progress * 100f):0}%";
+
+            // 이 때, 서버와 통신해서 유저 및 기획 데이터를 받아오면 된다.
+
+
+            if (asyncOperation.progress >= 0.9f)
+            {
+                _loadingSlider.value = 1;
+                _progressTMP.text = $"100%";
+                asyncOperation.allowSceneActivation = true; // 로딩이 완료되면 Scene을 활성화한다.
+            }
+            yield return null;
+        }
+    }
+}
+
+
